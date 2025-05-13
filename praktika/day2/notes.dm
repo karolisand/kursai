40ee4237f91ba337ed38c7f7e0bd51b84dceea93
karolisand <karolis.andrikonis@gmail.com>
2025-05-13 21:36:44 +0300

    pirmas youtube uzdavinys

---
+++ praktika/day2/notes.dm
@@ -0,0 +1,13 @@
+Ollama -> for AI chatbot use
+llama -> garsesnis modelis (open source)
+huggingface.co -> for AI source code
+Daugiausia reikia zinoti apie: gpt o3 vienas geriausiu mastanciu modeliu,
+
+
+
+
+
+
+Kas vyksta su parsisiustais ollama projektais? ar jie lieka kompe? kaip juos pa6alinti?
+Ar galima parsisiusta ollama isjungti kuryba?
+ar issaugota informacija lieka tik tavo account  lygiu ar persiduoda i visa modeli?
---
+++ praktika/day2/praktika1.py
@@ -0,0 +1,42 @@
+# Nuoroda: https://youtu.be/kqtD5dpn9C8?si=LM-uoUxN9aq-AP9j&t=2181
+#Destytojo galutinis veikiantis:
+weight = float(input("Weigh: "))
+measurement = input("(k)kg or (l)lb: ")
+
+if measurement.upper() == "K":
+    converted = weight / 0.45
+    print("weigh in LBs: " + str(converted))
+elif measurement.upper() == "L":
+    converted = weight * 0.45
+    print("weigh in KG: " + str(converted))
+else:
+    print("you need to choose l or k")
+
+
+
+# Mano galutinis veikiantis:
+# weight = input("Weigh: ")
+# measurement = input("(k)kg or (l)lb: ")
+
+# if measurement.upper() == "K":
+#     converted = float(weight) / 0.45
+#     print("weigh in LBs: " + str(converted))
+# elif measurement.upper() == "L":
+#     converted = float(weight) * 0.45
+#     print("weigh in KG: " + str(converted))
+# else:
+#     print("you need to choose l or k")
+
+
+
+
+# Pirmas dublis:
+# if measurement == str("l"):
+#     print("skaicius")
+# elif measurement == str("k"):
+#     print("bbz")
+
+print ("")
+print ("Done")
+
+#int(); float(); bool(); str()
