646bb4e6732bbe68f70f67242d192b0e8163b90b
karolisand <karolis.andrikonis@gmail.com>
2025-05-13 22:18:16 +0300

    Antro uzdavinio commit

---
+++ praktika/day2/praktika2.py
@@ -0,0 +1,65 @@
+# numbers = (1, 2, 3, 4, 5) -> negali modifikuoti ne skliaustai paprasti
+
+
+# numbers = range(5, 11, 2) #kas antra
+# for number in numbers:   #!!!! gali range naudoti tiesiai formulele
+#     print(number)
+
+
+# numbers = range(5, 11)
+# for number in numbers:
+#     print(number)
+
+
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print(item)
+# #Or the same but longer
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i + 1
+
+
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers)) #kiek yra vienetu liste
+
+
+# numbers = [1, 2, 3, 4, 5]
+# print(1 in numbers)
+
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.remove(3) #you can remove all numbers by numbers.clear()
+# print(numbers)
+
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.insert(0, -1)
+# print(numbers)
+
+
+# names = ["Karolis", "Monika", "Titas", "Ugnius"]
+# names[0] = "Karo"
+# print(names[0:3])
+
+
+# names = ["Karolis", "Monika"]
+# names[0] = "Karo"
+# print(names[0])
+
+
+# names = ["Karolis", "Monika"]
+# print(names[1])
+
+
+# i = 1
+# while i<=5:
+#     print(i * "*")
+#     i = i + 1
+
+
+# i = 1
+# while i<=5:
+#     print(i)
+#     i = i + 1
